e1d947444f40b57df411eab96139680dbd7bc118
leagerxd <misha.zabotin@gmail.com>
2023-06-19 16:57:51 +0300

    Using numpy to multiply matrices.
    using 3.10-bullseye version

--- Lab3/python/spaceship/routers/api.py
+++ Lab3/python/spaceship/routers/api.py
@@ -1,8 +1,31 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
-
 
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def matrix() -> dict:
+    matrixA = np.random.rand(5, 5)
+    matrixB = np.random.rand(5, 5)
+    
+    rowsA, columnsA = matrixA.shape
+    rowsB, columnsB = matrixB.shape
+
+    if columnsA != rowsB:
+        response = {
+            'error': 'Matrix dimensions are not compatible for multiplication'
+        }
+    else:
+        result = np.dot(matrixA, matrixB)
+        
+        response = {
+            'matrixA': matrixA.tolist(),
+            'matrixB': matrixB.tolist(),
+            'product': result.tolist()
+        }
+
+    return response
